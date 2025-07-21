91c51904e807d6a7de766192fde8128fef0e9500
Raheem-Baig1210 <raheembaig822@gmail.com>
2025-07-21 14:38:58 +0530

    Prime Number

---
+++ PrimeNo.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n,i,isPrime=true;
+    cout<<"Enter the number to check it is prime or not : ";
+    cin>>n;
+    for(i=2;i<n;i++){
+        if(n%i==0){
+            isPrime=false;
+        }
+    }
+    cout<<(isPrime ? "Given number is Prime Number":"Given number is Not a Prime Number");
+}
